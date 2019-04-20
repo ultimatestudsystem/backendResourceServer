df4cd1483184063cc0d265a23c1ae4dbe2a6e431
Oleg Bedrin <numeralhuman@gmail.com>
2019-04-21 00:15:31 +0300

    Adding freemarker templates

--- src/main/java/com/studsystem/controllers/DowloadUploadWithViewController.java
+++ src/main/java/com/studsystem/controllers/DowloadUploadWithViewController.java
@@ -1,14 +1,16 @@
 package com.studsystem.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DowloadUploadWithViewController {
 
     @GetMapping("/upload")
-    public String getUploadView() {
-        throw new RuntimeException("Not implemented yet");
+    public String getUploadView(Model model) {
+        model.addAttribute("message", "Welcome aboard!");
+        return "index";
     }
 
 }
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,17 @@
+<#import "/spring.ftl" as spring/>
+
+<!DOCTYPE HTML>
+<html>
+   <head>
+      <meta charset="UTF-8" />
+      <title>Welcome</title>
+   </head>
+
+   <body>
+      <h1>Welcome</h1>
+      <#if message??>
+      <h2>${message}</h2>
+      </#if>
+   </body>
+
+</html>
